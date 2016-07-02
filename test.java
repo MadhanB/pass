73d0bc2b44f516d0c0c0cd440be26643910d7019
MadhanB <smartmadhan07@gmail.com>
2016-07-02 16:28:37 +0530

    finish

---
+++ test.java
@@ -0,0 +1,10 @@
+# pass
+class test{
+public static voidmain(String[] args){
+int a=10;
+if(a>0){
+System.out.println("the number is positive"):}
+else if(a<0){
+System.out.println("the number is negative"):}
+elseSystem.out.println("the number is zero"):
+}}
